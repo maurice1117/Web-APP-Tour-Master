36e3eaaaa7a303ffe652d889028c298e0909ad87
Maurice <maurice8huang@gmail.com>
2024-12-07 16:38:49 +0800

    GPT Prompt更新

--- backend/search/GPT.py
+++ backend/search/GPT.py
@@ -8,7 +8,7 @@ client = openai.OpenAI(api_key = config.OPENAI_API_KEY)
 def generate_attractions(location):
 
     message = [
-        {'role':'system','content':'請根據給定的地點，提供地點周邊的9個旅遊景點名稱，並以JSON格式表示，格式如下：{"attraction": ["景點1", "景點2", ..., "景點9"]}。"'},
+        {'role':'system','content': '請根據給定的地點，提供該地區周邊真實且存在的9個旅遊景點名稱，並以JSON格式表示，格式如下：{"attraction": ["景點1", "景點2", ..., "景點9"]}。請勿虛構不存在的景點，並確保每個景點為該地區的熱門旅遊地點。'},
         {'role':'user','content':'台北市'},
         {'role':'assistant','content':'{"attraction": ["台北101","故宮博物院","中正紀念堂","士林夜市","大稻埕碼頭","龍山寺","國父紀念館","木柵動物園","迪化街"]}'},
         {'role':'user','content':location}
