7de35152a0102c6724549ed2c1707417c78dc895
Sankonsky <r12922156@ntu.edu.tw>
2024-12-17 21:16:16 +0800

    public version

--- backend/config.py
+++ backend/config.py
@@ -1,5 +1,5 @@
 # ChatGPT
-OPENAI_API_KEY = "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"
+OPENAI_API_KEY = "input your openai api key"
 GPT_MODEL = "gpt-4o-mini"
 
 # Search Method
@@ -7,8 +7,8 @@ GPT_MODEL = "gpt-4o-mini"
 SEARCH_ENGINE = "BING"
 
 # Google Search
-GOOGLE_API_KEY = "AIzaSyCOk5l-qbk30b73aSMBhcsPsqRvoCy6VyY"
-GOOGLE_ENGINE_ID = "b78fdff937c5f45aa"
+GOOGLE_API_KEY = "input your google api key"
+GOOGLE_ENGINE_ID = "input your engine id"
 
 # Bing Search
-BING_API_KEY = "f05fccea3da44b0b8c2066f569c70ffe"
+BING_API_KEY = "input your bing api key"
